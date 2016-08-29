f8cd03fb6cac44cbc2711de0a23aa5937a26e604
Chelsey Smith <mjilnor@hotmail.com>
2016-08-28 21:31:41 -0600

    Added new scene to test ai concepts.

--- Assets/Scripts/DEBUG_display.cs
+++ Assets/Scripts/DEBUG_display.cs
@@ -29,7 +29,7 @@ public class DEBUG_display : MonoBehaviour {
         Rect rect = new Rect(0, 0, w, h * 2 / 100);
         style.alignment = TextAnchor.UpperLeft;
         style.fontSize = h * 2 / 100;
-        style.normal.textColor = new Color(0.0f, 0.0f, 0.5f, 1.0f);
+        style.normal.textColor = new Color(1f, 1f, 1f, 1.0f);
         float msec = deltaTime * 1000.0f;
         float fps = 1.0f / deltaTime;
         string text = string.Format("{0:0.0} ms ({1:0.} fps)", msec, fps);
@@ -49,7 +49,7 @@ public class DEBUG_display : MonoBehaviour {
         Rect rect = new Rect(0, 20, w, h * 2 / 100);
         style.alignment = TextAnchor.UpperLeft;
         style.fontSize = h * 2 / 100;
-        style.normal.textColor = new Color(0.0f, 0.0f, 0.5f, 1.0f);
+        style.normal.textColor = new Color(1f, 1f, 1f, 1.0f);
         string text = "Cubes: " + cube_count.ToString();
         GUI.Label(rect, text, style);
     }
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+    private Rigidbody m_rbody;
+    private GameObject m_camera;
+    public float force = 10;
+    // Use this for initialization
+    void Start () {
+        m_rbody = GetComponent<Rigidbody>();
+        m_camera = GameObject.Find("Main Camera");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //GET JOYSTICK IMPUT AND APPLY MOVEMENT
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        m_rbody.AddForce(new Vector3(x * force, 0.0f, z * force));
+
+        //MOVE CAMERA ALONG WITH PLAYER
+        m_camera.transform.position = new Vector3(m_rbody.transform.position.x, m_rbody.transform.position.y + 2, m_rbody.transform.position.z - 9f);
+
+    }
+}
